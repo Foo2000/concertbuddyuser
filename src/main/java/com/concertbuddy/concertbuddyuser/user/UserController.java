85b2765594dbf5f057a216059eea62334860d8f6
Zhengwen Fu <zfmyac@gmail.com>
2023-10-12 22:16:50 -0700

    added Spring HATEOAS

--- src/main/java/com/concertbuddy/concertbuddyuser/user/UserController.java
+++ src/main/java/com/concertbuddy/concertbuddyuser/user/UserController.java
@@ -1,10 +1,21 @@
 package com.concertbuddy.concertbuddyuser.user;
 
+import com.concertbuddy.concertbuddyuser.song.Song;
+import com.concertbuddy.concertbuddyuser.song.SongController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path = "api/v1/users")
@@ -18,13 +29,31 @@ public class UserController {
     }
 
     @GetMapping
-    public List<User> getUsers() {
-        return userService.getUsers();
+    public ResponseEntity<CollectionModel<EntityModel<User>>> getUsers() {
+        List<EntityModel<User>> usersWithLinks = userService.getUsers().stream().map(
+                user -> EntityModel.of(
+                        user,
+                        // list of song links
+                        user.getSongs().stream().map(
+                                // map song to link
+                                song -> linkTo(methodOn(SongController.class).getSongById(song.getId())).withRel("song: "+song.getName())
+                        ).toList())
+        ).toList();
+
+        return ResponseEntity.ok(CollectionModel.of(usersWithLinks));
     }
 
     @GetMapping(path="{userId}")
-    public User getUserById(@PathVariable("userId") UUID userId) {
-        return userService.getUserById(userId);
+    public ResponseEntity<EntityModel<User>> getUserById(@PathVariable("userId") UUID userId) {
+        User user = userService.getUserById(userId);
+        EntityModel<User> userWithLinks = EntityModel.of(
+                user,
+                // list of song links
+                user.getSongs().stream().map(
+                        // map song to link
+                        song -> linkTo(methodOn(SongController.class).getSongById(song.getId())).withRel("song: "+song.getName())
+                ).toList());
+        return ResponseEntity.ok(userWithLinks);
     }
 
     @PostMapping
